0a31a0509597cafa208d3984608247be3eb3c766
Alfredo <dytalfredomendoza@gmail.com>
2023-09-11 20:12:37 -0400

    Prueba 2

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { validateMovieRequestBody, validatePartialMovie } from '../schemas/movies.js'
-import { randomUUID } from 'node:crypto'
+// import { validateMovieRequestBody, validatePartialMovie } from '../schemas/movies.js'
+// import { randomUUID } from 'node:crypto'
 import { MovieModel } from '../models/movies.js'
 
 export const moviesRouter = Router()
@@ -18,47 +18,47 @@ moviesRouter.get('/:id', (req, res) => {
   res.status(404).json({ message: 'Movie no existe' })
 })
 
-moviesRouter.post('/', (req, res) => {
-  const result = validateMovieRequestBody(req.body)
-  if (result.error) {
-    // 422 bad request enviado mal los datos
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data
+// moviesRouter.post('/', (req, res) => {
+//   const result = validateMovieRequestBody(req.body)
+//   if (result.error) {
+//     // 422 bad request enviado mal los datos
+//     return res.status(422).json({ error: JSON.parse(result.error.message) })
+//   }
+//   const newMovie = {
+//     id: randomUUID(),
+//     ...result.data
 
-  }
-  console.log(newMovie)
+//   }
+//   console.log(newMovie)
 
-  movies.push(newMovie)
-  res.status(201).json(newMovie)
-})
+//   movies.push(newMovie)
+//   res.status(201).json(newMovie)
+// })
 
-moviesRouter.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-  if (movieIndex === -1) { return res.status(404).json({ message: 'Movie no found' }) }
-  console.log('ARTICULO BORRADO', movies[movieIndex].title)
-  movies.splice(movieIndex, 1)
+// moviesRouter.delete('/:id', (req, res) => {
+//   const { id } = req.params
+//   const movieIndex = movies.findIndex(movie => movie.id === id)
+//   if (movieIndex === -1) { return res.status(404).json({ message: 'Movie no found' }) }
+//   console.log('ARTICULO BORRADO', movies[movieIndex].title)
+//   movies.splice(movieIndex, 1)
 
-  return res.json({ message: 'Movie Delete' })
-})
+//   return res.json({ message: 'Movie Delete' })
+// })
 
-moviesRouter.patch('/:id', (req, res) => {
-  const result = validatePartialMovie(req.body)
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie no found' })
+// moviesRouter.patch('/:id', (req, res) => {
+//   const result = validatePartialMovie(req.body)
+//   if (!result.success) {
+//     return res.status(400).json({ error: JSON.parse(result.error.message) })
+//   }
+//   const { id } = req.params
+//   const movieIndex = movies.findIndex(movie => movie.id === id)
+//   if (movieIndex === -1) return res.status(404).json({ message: 'Movie no found' })
 
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-  movies[movieIndex] = updateMovie
+//   const updateMovie = {
+//     ...movies[movieIndex],
+//     ...result.data
+//   }
+//   movies[movieIndex] = updateMovie
 
-  res.status(200).json(updateMovie)
-})
+//   res.status(200).json(updateMovie)
+// })
